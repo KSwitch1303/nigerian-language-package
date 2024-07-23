4e1c120f4d906f4f768c04921bb5b9094fba72f4
Favor Misongo <ebimieteimisongo@gmail.com>
2024-07-23 22:53:34 +0100

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,103 @@
-function ikwo(string) {
-  return console.log(string);
+const variables ={}
+const functions = {};
+
+//console.log
+function talk(string) {
+  console.log(string);
 }
 
-function maka(initial, constrain, body) {
+//For Loop
+function forLoop(initial, constrain, body) {
   for(i = initial; i < constrain; i++) {
-    console.log(body);
+    body(i);
   }
 }
 
+//Create Variable
+function createVariable(name, value) {
+  variables[name] = value;
+}
+
+//Create Function
+function createFunction(functionName, params, body) {
+  const func = new Function(params, body);
+  functions[functionName] = func;
+}
+
+//if Statement
+function ifStatement(condition, trueBody, falseBody = '') {
+  const func = new Function('aha', 'kwou', 'oruko', 'soro', `
+    if (${condition}) { 
+      ${trueBody}
+    } else {
+      ${falseBody} }`);
+  func(variables, talk, variables, talk);
+}
+
+//Random
+
+function randomGen(range) {
+  return Math.floor(Math.random() * range);
+}
+
+
+//While Loop
+function whileLoop(condition, body) {
+  const func = new Function('aha', 'kwou', `
+    while (${condition}) { 
+      ${body} 
+    }
+  `);
+  func(variables, talk);
+}
+
+//Igbo
+
+const igbo = {
+  aha: variables, // global variables
+  oru_: functions, // global functions
+  kwou: talk, // console.log
+  maka: forLoop, // for loop
+  ka: createVariable, // create variable
+  oru: createFunction, // create function
+  o_buru: ifStatement, // if statement
+  gbasasia: randomGen, // random
+  mgbe: whileLoop // while loop
+}
+
+
+//Yoruba
+
+//console.log
+const yoruba = {
+  oruko: variables, // global variables
+  ise_: functions, // global functions
+  soro: talk, // console.log
+  fun: forLoop, // for loop
+  bari: createVariable, // create variable
+  ise: createFunction, // create function
+  ipo: ifStatement, // if statement
+  laileto: randomGen, // random
+  nigba_ti: whileLoop // while loop
+}
+
+// Hausa
+const hausa = {
+  suna: variables, // global variables
+  aiki_: functions, // global functions
+  magana: talk, // console.log
+  domin: forLoop, // for loop
+  je_ki: createVariable, // create variable
+  aiki: createFunction, // create function
+  yanayi: ifStatement, // if statement
+  bazuwar: randomGen, // random
+  yayin_da: whileLoop // while loop
+}
+
+
+
 module.exports = {
-  ikwo,
-  maka
+  igbo,
+  yoruba,
+  hausa
 }
